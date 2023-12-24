7daa31da8c711ace128eeb3f278e7d12b351f138
faishal-developer <foysal.professional@gmail.com>
2023-12-24 23:44:11 +0600

    build

--- src/components/category/utils/categoryInterface.tsx
+++
@@ -1,9 +0,0 @@
-export type ICategoryStatus = 'active' | 'inactive';
-
-export type ICategory = {
-    _id?: string;
-    id?: string;
-    name: string;
-    desc?: string;
-    status: ICategoryStatus;
-};
